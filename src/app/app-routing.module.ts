0efcdec9089b769c83a49d7dbce3f3b6bb72198e
Vishal Sharma <vishal.sharma@finefarefood.com>
2025-08-11 22:01:29 +0530

    initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { AuthGuard } from "./core/guards/auth.guard";
+import { LayoutComponent } from "./layouts/layout.component";
+import { CyptolandingComponent } from "./cyptolanding/cyptolanding.component";
+import { Page404Component } from "./extrapages/page404/page404.component";
+
+const routes: Routes = [
+  {
+    path: "auth",
+    loadChildren: () =>
+      import("./account/account.module").then((m) => m.AccountModule),
+  },
+  {
+    path: "",
+    component: LayoutComponent,
+    loadChildren: () =>
+      import("./pages/pages.module").then((m) => m.PagesModule),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: "pages",
+    loadChildren: () =>
+      import("./extrapages/extrapages.module").then((m) => m.ExtrapagesModule),
+    canActivate: [AuthGuard],
+  },
+  { path: "crypto-ico-landing", component: CyptolandingComponent },
+  { path: "**", component: Page404Component },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: "top" })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/shared/ui/ui.module.ts
@@ -0,0 +1,23 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TimepickerModule } from "ngx-bootstrap/timepicker";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+
+import { PagetitleComponent } from "./pagetitle/pagetitle.component";
+import { LoaderComponent } from "./loader/loader.component";
+@NgModule({
+  declarations: [PagetitleComponent, LoaderComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BsDatepickerModule.forRoot(),
+    TimepickerModule.forRoot(),
+    BsDropdownModule.forRoot(),
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  exports: [PagetitleComponent, LoaderComponent],
+})
+export class UIModule {}
